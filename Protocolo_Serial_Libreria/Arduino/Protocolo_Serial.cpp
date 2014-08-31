d67a3d537e8df5843082a97ce6a64eadba023811
ddtdanilo <danilodt@gmail.com>
2014-08-31 12:10:30 -0430

    union

--- Protocolo_Serial_Libreria/Arduino/Protocolo_Serial.cpp
+++ Protocolo_Serial_Libreria/Arduino/Protocolo_Serial.cpp
@@ -1,10 +1,10 @@
 /*
 	Creada por Danilo Diaz T, 27 de Agosto, 2014
 	Para FutBot USB y de dominio público
-*/
+	*/
 
-#include Arduino.h
-#include Protocolo_Serial.h
+	#include Arduino.h
+	#include Protocolo_Serial.h
 
 /*
 pX is a pointer to another type X; this contains very little semantic information.
@@ -13,7 +13,7 @@ dY might represent a distance along the Y-axis of a graph, while a variable just
 sz is a null- or zero-terminated string. In C, this contains some semantic information because it is not clear whether a variable of type char* is a pointer to a single character, an array of characters or a zero-terminated string.
 w marks a variable that is a word. This contains essentially no semantic information at all, and would probably be considered Systems Hungarian.
 b marks a byte, which in contrast to w might have semantic information, because in C the only byte-sized data type is the char, so these are sometimes used to hold numeric values. This prefix might clear ambiguity between whether the variable is holding a value that should be treated as a character or a number.
-fp means floating point (Punto FLotante)
+fp means floating point (Punto Flotante)
 i means integer (entero)
 U means union
 */
@@ -22,18 +22,22 @@ U means union
 /*
  Los tipo union te permiten tomar un espacio de la memoria y verla de formas distintas, en el caso 
  que sigue a continuación, se tiene un arreglo de 2 bytes (********!********) y ese mismo espacio de memoria,
- se puede ver como una palabra (word) de 16 bits (2 bytes) (********!********)
+	 se puede ver como una palabra (word) de 16 bits (2 bytes) (********!********)
 
  De esta manera, se pueden tomar palabras y enviarlas como bytes de manera cómoda. De manera análoga, se realiza el punto flotante, sólo
  que cambia la manera de interpretarse el dato en el que recibe
+ */
+
+ typedef union{
+ 	byte bData[2];
+ 	word wData;
+ 	}UiData;
+
+/*
+análogo a lo anterior
 */
 typedef union{
-			byte bData[2];
-			word wData;
-		}UiData;
+	float fpData;
+	word wData;
+	}UfpData;
 
-
-typedef union{
-		float fpData;
-		word wData;
-		}UfpData;
---
+++ Protocolo_Serial_Libreria/Arduino/Protocolo_Serial.h
@@ -0,0 +1,16 @@
+#ifndef Protocolo_Serial_h
+#define Protocolo_Serial_h
+
+#include "Arduino.h"
+
+class Serial.find(target);
+{
+  public:
+    Morse(int pin);
+    void dot();
+    void dash();
+  private:
+    int _pin;
+};
+
+#endif
